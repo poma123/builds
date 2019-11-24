ae507811161b66e5503b604c1c2175b8d95e6b29
Poma123 <vhvvvhhh@gmail.com>
2019-11-24 13:53:38 +0100

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,4 @@
-git config user.name "TheBusyBot"
+git config user.name "ThePomaBot"
 git config user.email ${LOGIN_EMAIL}
 
-git remote set-url origin https://${ACCESS_TOKEN}@github.com/TheBusyBiscuit/builds.git
+git remote set-url origin https://${ACCESS_TOKEN}@github.com/poma123/builds.git
