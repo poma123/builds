93685d804fcd4239058dd7a321da7b4cb651dec0
Poma123 <vhvvvhhh@gmail.com>
2019-11-25 20:15:53 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -96,7 +96,7 @@ function check(job, logging) {
             var timestamp = parseInt(commit.commit.committer.date.replace(/\D/g, ""));
 
             if (commit.commit.message.toLowerCase().startsWith("[ci skip]")) {
-                return Promise.reject(new Error('Skipping build...'));
+                return Promise.reject(new Error('Skipping build...')).then(resolve, reject);
             }
 
             job.commit = {
